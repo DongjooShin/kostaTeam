16a8631bea1c2c3fa10f47f236a772801ae26ff5
kkt891230 <kkt891230@naver.com>
2016-04-21 10:15:07 +0900

    Apart basic setting!

---
+++ main/src/apt/dao/AptService.java
@@ -0,0 +1,15 @@
+package apt.dao;
+
+public class AptService {
+	// 동주
+	
+	// 정화
+	
+	// 수연
+	
+	// 경태
+	
+	// 기대
+	
+	// 경운
+}
---
+++ main/src/apt/mapper/AptMapper.java
@@ -0,0 +1,5 @@
+package apt.mapper;
+
+public interface AptMapper {
+
+}
